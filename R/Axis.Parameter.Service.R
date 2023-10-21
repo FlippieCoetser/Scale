7bd5396759ca88ee9d1ab2488b68be8d1a439064
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 09:16:56 -0700

    SERVICE: service[['Get.X.Start']]() ReturnsXAxisStart -> PASS

--- R/Axis.Parameter.Service.R
+++ R/Axis.Parameter.Service.R
@@ -1,5 +1,7 @@
 Axis.Parameter.Service <- \() {
   services <- list()
-  services[['Get.X.Start']] <- \() {}
+  services[['Get.X.Start']] <- \() {
+    par('usr')[1:2][[1]]
+  }
   return(services)
 }
