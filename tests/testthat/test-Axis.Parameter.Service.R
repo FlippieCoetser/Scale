bfd5304bbb1350f6b546385220bb12c41e8ca53d
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 09:02:44 -0700

    SERVICE: Axis.Parameter.Service() ReturnsListOfServices -> FAIL

--- tests/testthat/test-Axis.Parameter.Service.R
+++ tests/testthat/test-Axis.Parameter.Service.R
@@ -3,3 +3,13 @@ describe('Axis.Parameter.Service',{
     Axis.Parameter.Service |> expect.exist()
   })
 })
+
+describe("When services <- Axis.Parameter.Service()",{
+  it("then services is a list",{
+    # When
+    services <- Axis.Parameter.Service()
+
+    # Then
+    services |> expect.list()
+  })
+})
