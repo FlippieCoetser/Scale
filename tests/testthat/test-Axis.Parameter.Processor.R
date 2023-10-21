c6204fcd028cb3e3c370fa6a2baa0be3ecb5c2bf
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 10:17:25 -0700

    PROCESSING: process[['Get.X.Range']]() ReturnsXAxisRange -> FAIL

--- tests/testthat/test-Axis.Parameter.Processor.R
+++ tests/testthat/test-Axis.Parameter.Processor.R
@@ -20,3 +20,19 @@ describe("When processors <- service |> Axis.Parameter.Processor()",{
     processors[['Get.X.Range']] |> expect.exist()
   })
 })
+
+describe("When process[['Get.X.Range']]()",{
+  it("then service[['Get.X.End']]() - service[['Get.X.Start']]() is returned",{
+    # Given
+    service <- Axis.Parameter.Service()
+    process <- service |> Axis.Parameter.Processor()
+
+    expected.x.range <- service[['Get.X.End']]() - service[['Get.X.Start']]()
+
+    # When
+    actual.x.range <- process[['Get.X.Range']]()
+
+    # Then
+    actual.x.range |> expect.equal(expected.x.range)
+  })
+})
