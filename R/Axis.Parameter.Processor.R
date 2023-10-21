e092a42a51a56f93975a7028ff7701c5cf5eb90a
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 10:17:31 -0700

    PROCESSING: process[['Get.X.Range']]() ReturnsXAxisRange -> PASS

--- R/Axis.Parameter.Processor.R
+++ R/Axis.Parameter.Processor.R
@@ -1,5 +1,7 @@
 Axis.Parameter.Processor <- \(service) {
   processors <- list()
-  processors[['Get.X.Range']] <- \() {}
+  processors[['Get.X.Range']] <- \() {
+    service[['Get.X.End']]() - service[['Get.X.Start']]()
+  }
   return(processors)
 }
