855a464858a90d55f40a8c9547a09e87de9c0600
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 11:59:59 -0700

    DOCUMENTATION: Update Documents

--- R/Scaler.R
+++ R/Scaler.R
@@ -1,3 +1,17 @@
+#' Scaler'
+#' 
+#' @description
+#' Scale X and Y Coordinates given a maximum x and y value passed as parameters
+#'  
+#' @usage NULL
+#' @param parameters A data.frame containing the following columns:
+#' * `x` - The maximum x value
+#' * `y` - The maximum y value
+#' @returns a list containing two scaling utilities: 
+#' * `scale <- parameters |> Scaler()`
+#' * `x |> scale[['X']]()`
+#' * `y |> scale[['Y']]()`
+#' @export
 Scaler <- \(parameters) {
   axis <- Axis.Parameter.Service() |> Axis.Parameter.Processor()
 
