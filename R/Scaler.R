c982bf05187a7d30f64fa345fd2c1559481781b7
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 11:52:30 -0700

    ORCHESTRATION: x |> scale[['X']]() SalesX -> PASS

--- R/Scaler.R
+++ R/Scaler.R
@@ -1,6 +1,10 @@
 Scaler <- \(parameters) {
+  axis <- Axis.Parameter.Service() |> Axis.Parameter.Processor()
+
   utilities <- list()
-  utilities[['X']] <- \() {}
+  utilities[['X']] <- \(x) {
+    (x / parameters[['x']]) * axis[['Get.X.Range']]()
+  }
   utilities[['Y']] <- \() {}
   return(utilities)
 }
