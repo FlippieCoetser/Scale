f9c74bf3b85df1f81dd80a71e277b0b66c5d5c35
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-21 09:50:49 -0700

    CODE RUB: Update Unit Test

--- tests/testthat/test-Axis.Parameter.Service.R
+++ tests/testthat/test-Axis.Parameter.Service.R
@@ -88,11 +88,11 @@ describe("When service[['Get.Y.Start']]()",{
 })
 
 describe("When service[['Get.Y.End']]()",{
-  it("then the x end position of the plot area is returned",{
+  it("then the y end position of the plot area is returned",{
     # Given
     service <- Axis.Parameter.Service()
 
-    expected.x.end <- par('usr')[3:4][[2]]
+    expected.y.end <- par('usr')[3:4][[2]]
 
     # When
     actual.y.end <- service[['Get.Y.End']]()
