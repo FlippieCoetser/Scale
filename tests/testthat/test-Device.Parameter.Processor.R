177f8b708b2d7143526f2ab38670c752f5b57731
Flippie Coetser <flippie.coetser@gmail.com>
2024-01-29 05:31:55 -0800

    PROCESSOR: service |> Device.Parameter.Processor() ReturnsListOfProcessors -> FAIL

--- tests/testthat/test-Device.Parameter.Processor.R
+++ tests/testthat/test-Device.Parameter.Processor.R
@@ -3,3 +3,16 @@ describe('Device.Parameter.Processor',{
     Device.Parameter.Processor |> expect.exist()
   })
 })
+
+describe("When processors <- service |> Device.Parameter.Processor()", {
+  it("then processors is a list", {
+    # Given
+    service <- Device.Parameter.Service()
+
+    # When
+    processors <-  service |> Device.Parameter.Processor()
+
+    # Then
+    processors |> expect.list()
+  })
+})
