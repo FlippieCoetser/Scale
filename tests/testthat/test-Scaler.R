2cb583ebe03669e08fc7c4f04126aca524c11122
Flippie Coetser <flippie.coetser@gmail.com>
2024-01-29 06:05:26 -0800

    COMPONENT: parameter |> Scaler(source='device') ReturnsScalingUtilitiesWhichUseDeviceSize -> FAIL

--- tests/testthat/test-Scaler.R
+++ tests/testthat/test-Scaler.R
@@ -225,3 +225,95 @@ describe("When utilities <- parameters |> Scaler(source = 'device')",{
     utilities |> expect.list()
   })
 })
+
+describe("When source = device and x |> scale[['X']]()",{
+  it("then x is scaled using parameters and x range",{
+    # Given
+    parameters <- data.frame(
+      x = 3000,
+      y = 2500
+    )
+
+    processor <- Device.Parameter.Service() |> Device.Parameter.Processor()
+
+    x.range <- processor[['Get.X.Range']]()
+
+    x <- 2700
+
+    expected.x <- (x / parameters[['x']]) * x.range
+
+    scale <- parameters |> Scaler(source = 'device')
+
+    # When
+    actual.x <- x |> scale[['X']]()
+
+    # Then
+    actual.x |> expect.equal(expected.x)
+  })
+})
+
+describe("When source = device and y |> scale[['Y']]()",{
+  it("then y is scaled using parameters and y range",{
+    # Given
+    parameters <- data.frame(
+      x = 3000,
+      y = 2500
+    )
+
+    processor <- Device.Parameter.Service() |> Device.Parameter.Processor()
+
+    y.range <- processor[['Get.Y.Range']]()
+
+    y <- 2000
+
+    expected.y <- (y / parameters[['y']]) * y.range
+
+    scale <- parameters |> Scaler(source = 'device')
+
+    # When
+    actual.y <- y |> scale[['Y']]()
+
+    # Then
+    actual.y |> expect.equal(expected.y)
+  })
+})
+
+describe("When source = device and coordinates |> scale[['Coordinates']]()",{
+  it("then coordinates is scaled using parameters and device ranges",{
+    # Given
+    parameters <- data.frame(
+      x = 3000,
+      y = 2500
+    )
+
+    processor <- Device.Parameter.Service() |> Device.Parameter.Processor()
+
+    x.range <- processor[['Get.X.Range']]()
+    y.range <- processor[['Get.Y.Range']]()
+
+
+    x <- c(0,2000,2000,0,0)
+    y <- c(0,0,1000,1000,0)
+
+    coordinates <- data.frame(
+      x = x,
+      y = y
+    )
+
+    expected.x <- (x / parameters[['x']]) * x.range
+    expected.y <- (y / parameters[['y']]) * y.range
+
+    expected.coordinates <- data.frame(
+      x = expected.x,
+      y = expected.y
+    )
+
+    scale <- parameters |> Scaler(source = 'device')
+
+    # When
+    actual.coordinates <- coordinates |> scale[['Coordinates']]()
+
+    # Then
+    actual.coordinates |> expect.equal(expected.coordinates)
+  })
+})
