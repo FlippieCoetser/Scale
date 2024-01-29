fcfccf987b6647f079c09c36e08e7eecedae5a2b
Flippie Coetser <flippie.coetser@gmail.com>
2024-01-29 04:54:54 -0800

    SERVICE: Device.Parameter.Service() ServiceReturnsDeviceSize -> PASS

--- R/Device.Parameter.Service.R
+++ R/Device.Parameter.Service.R
@@ -1,1 +1,10 @@
-Device.Parameter.Service <-  \() {}
+Device.Parameter.Service <-  \() {
+  services <- list()
+  services[['Get.Width']] <- \(unit = 'px') {
+      dev.size(unit)[1] 
+  }
+  services[['Get.Height']] <- \(unit = 'px') {
+      dev.size(unit)[2] 
+  }
+  services
+}
