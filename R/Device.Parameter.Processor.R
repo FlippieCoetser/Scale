bf3e260d30fb7aed0522f2b7679f30779e5d8e0c
Flippie Coetser <flippie.coetser@gmail.com>
2024-02-04 06:31:49 -0800

    PROCESSING: Device.Parameter.Processor() ReturnsListContainingGet.Norm.X.RangeProcessor -> PASS

--- R/Device.Parameter.Processor.R
+++ R/Device.Parameter.Processor.R
@@ -3,6 +3,8 @@ Device.Parameter.Processor <- \(service) {
   processors[['Get.X.Range']] <- \() {
     service[['Get.Width']]()
   }
+  processors[['Get.Norm.X.Range']] <- \() {
+  }
   processors[['Get.Y.Range']] <- \() {
     service[['Get.Height']]()
   }
