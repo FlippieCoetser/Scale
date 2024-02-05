f9885b5d74938bc5f9edabb9a5b76de829085b03
Flippie Coetser <flippie.coetser@gmail.com>
2024-02-05 03:07:23 -0800

    PROCESSING: Device.Parameter.Service() ReturnsListContainingGet.Aspect.RatioProcessor -> PASS

--- R/Device.Parameter.Processor.R
+++ R/Device.Parameter.Processor.R
@@ -1,5 +1,7 @@
 Device.Parameter.Processor <- \(service) {
   processors <-  list()
+  processors[['Get.Aspect.Ratio']] <- \() { 
+  }
   processors[['Get.X.Range']] <- \() {
     service[['Get.Width']]()
   }
